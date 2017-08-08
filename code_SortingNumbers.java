0d78eeca0fd05e14a8f1fe8fba3b3177279868fd
Purgatorius <Bartlomiej.Bugala@gmail.com>
2017-08-08 15:36:46 +0200

    Update code_SortingNumbers.java

--- code_SortingNumbers.java
+++ code_SortingNumbers.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
 /**
- * Created by B4RT on 2017-06-07.
+ * Created by B4RT on 2017-08-08.
  */
-public class Main {
-    public static void main(String[] args)
+public class SortingNumbers {
+    public static void main (String[] args)
     {
-        System.out.println("Hi! Give me three numbers from 1 to 100 and I'll arrangement it from the smallest to the largest");
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        System.out.println("----------------------");
-        System.out.println("Ok, it'll be that way:");
+        System.out.println("User enter three numbers between 1-1000. Program is going to arrange it in ascending order");
+        System.out.println("Give me yours three numbers: ");
 
-        for(int i = 0; i<=100; i++)
+        Scanner scanner = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Number one is: ");
+        a = scanner.nextInt();
+        System.out.println("Number two is: ");
+        b = scanner.nextInt();
+        System.out.println("And number three iiiss: ");
+        c = scanner.nextInt();
+
+        System.out.println("In ascending order it'll be that way: ");
+
+        for (int i = 0; i <= 1000; i++)
         {
             if (a==i)
                 System.out.println(a + " ");
@@ -22,6 +28,9 @@ public class Main {
                 System.out.println(b + " ");
             if (c==i)
                 System.out.println(c + " ");
+
         }
+
+        System.out.println("Thank you, have a nice day.");
     }
 }
